af53d97dca43ff09dc28bfe69c7c6b4edc668ed4
jhoogstraat <theenginej@gmail.com>
2018-07-19 13:29:05 +0200

    minor changes

--- extensions/FuzzySystem.js
+++ extensions/FuzzySystem.js
@@ -1,6 +1,7 @@
-export class FuzzySystem {
+class FuzzySystem {
 
-    constructor() {
+    constructor(defuzzyMethod) {
+        this.defuzzyMethod = defuzzyMethod
         this.inputVariables = {};
         this.outputVariable = [];
         this.rules = [];
@@ -22,33 +23,40 @@ export class FuzzySystem {
         var outputValues = []
         this.rules.forEach((rule) => { // Iterate through each rule
             var minValue = 1 // Start out at maximum Value for a LinguisticTerm -> AND-OPERATOR chooses least active.
-            for (var x in values) { // For every InputVariable
+            for (var x in values) { // For every InputVariable Description
                 var inputVariable = this.inputVariables[x] // Get the matching InputVariable
-                var variableTermRuledescription = rule.terms[x] // Get the matching rule description for the InputVariable
-                var term = inputVariable.terms[variableTermRuledescription] // Get the Lingustic Term (function) for the rule description
-                minValue = Math.min(minValue, term.values.y(values[x]))// AND-Operator
+                var variableTermRuleDescription = rule.terms[x] // Get the matching rule description for the InputVariable (eg. "niedrig, "mittel", etc.)
+                var term = inputVariable.terms[variableTermRuleDescription] // Get the Lingustic Term (function) for the rule description
+                minValue = Math.min(minValue, term.func.y(values[x]))// AND-Operator
             }
             outputValues.push(minValue)
         })
 
-        return this.computeCentroidPoint(outputValues)
+        return this.defuzzyMethod(this, outputValues)
     }
 
+    //---------
+    // Defuzzyfication Methods //
+    //---------
+
     // Does a poor-mans integral by taking the max value of each rules term description matching in outputValue yValue.
-    computeCentroidPoint(values) {
+}
+
+class DefuzzyficationMethods {
+    static centroidMethod(fuzzySystem, values) {
         var xsZaehler = 0;
         var xsNenner = 0;
 
-        var startValue = this.outputVariable.range.min
-        var endValue = this.outputVariable.range.max
+        var startValue = fuzzySystem.outputVariable.range.min
+        var endValue = fuzzySystem.outputVariable.range.max
 
         var dx = (endValue - startValue) / 150 // range evenly distributed into 250 wide steps.
 
         for (var xValue = startValue; xValue <= endValue; xValue += dx) { // Poor-mans Integral (but precision can be adjusted for the cost of compute time)
 
             var maxValue = 0; //Start out with minimum value -> OR-Operator chooses maximum value
-            for (var x in this.rules) { // Iterate through each rule (again)
-                var calculatedY = this.outputVariable.terms[this.rules[x].resultTerm].values.y(xValue)
+            for (var x in fuzzySystem.rules) { // Iterate through each rule (again)
+                var calculatedY = fuzzySystem.outputVariable.terms[fuzzySystem.rules[x].resultTerm].func.y(xValue)
                 maxValue = Math.max(maxValue, Math.min(values[x], calculatedY)) // We want the maximum of all output term values (for each rule). Each OutputTerm has a function, but is capped at the maximum value of the inference (min-operator here).
             }
 
@@ -56,11 +64,9 @@ export class FuzzySystem {
             xsNenner += maxValue
         }
 
-        console.log("Schwerpunkt für " + this.outputVariable.description + ": " + xsZaehler / xsNenner)
+        console.log("Schwerpunkt für " + fuzzySystem.outputVariable.description + ": " + xsZaehler / xsNenner)
         return xsZaehler / xsNenner
     }
-
-
 }
 
 class LinguisticVariable {
@@ -75,9 +81,9 @@ class LinguisticVariable {
 }
 
 class LinguisticTerm {
-    constructor(description, values) {
+    constructor(description, func) {
         this.description = description
-        this.values = values
+        this.func = func
     }
 }
 
@@ -100,15 +106,15 @@ class Triangular {
         this.left = array[0]
         this.top = array[1]
         this.right = array[2]
+        this.risingSlope = 1 / (this.top - this.left)
+        this.fallingSlope = 1 / (this.top - this.right)
     }
 
     y(xValue) {
         if (xValue <= this.top && this.top - this.left > 0) {
-            var m = 1 / (this.top - this.left)
-            return Math.max(0, m * xValue + -m * this.left)
+            return Math.max(0, this.risingSlope * xValue + -this.risingSlope * this.left)
         } else {
-            var m = 1 / (this.top - this.right)
-            return Math.max(0, m * xValue + -m * this.right)
+            return Math.max(0, this.fallingSlope * xValue + -this.fallingSlope * this.right)
         }
     }
 }
@@ -119,25 +125,26 @@ class Trapezoidal {
         this.topLeft = array[1]
         this.topRight = array[2]
         this.right = array[3]
+
+        this.risingSlope = 1 / (this.topLeft - this.left)
+        this.fallingSlope = 1 / (this.topRight - this.right)
     }
 
     y(xValue) {
         if (xValue < this.topLeft) {
             if (this.left == this.topLeft) return 0
-            var m = 1 / (this.topLeft - this.left)
-            return Math.max(0, m * xValue + -m * this.left)
+            return Math.max(0, this.risingSlope * xValue + -this.risingSlope * this.left)
         } else if (xValue <= this.topRight) {
             return 1
         } else {
             if (this.topRight == this.right) return 0
-            var m = 1 / (this.topRight - this.right)
-            return Math.max(0, m * xValue + -m * this.right)
+            return Math.max(0, this.fallingSlope * xValue + -this.fallingSlope * this.right)
         }
     }
 }
 
 
-var fuzzySystem = new FuzzySystem();
+var fuzzySystem = new FuzzySystem(DefuzzyficationMethods.centroidMethod);
 
 fuzzySystem.addInput(
     new LinguisticVariable("Umschlagszahl", new Range(0, 12), [
@@ -220,18 +227,18 @@ fuzzySystem.addRules(
 // fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 15.5, Umschlagszahl: 1.95 }) // Yilmaz: 19.7550 || Matlab: 18.1842
 
 var results = [];
-results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 6.3, Umschlagszahl: 8.9}) - 13.1330) // Yilmaz: 11.9995 || Matlab: 13.3430
-results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 6.3, Umschlagszahl: 8.9 }) - 13.3430) // Yilmaz: 11.9995 || Matlab: 13.3430
-results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 15.5, Umschlagszahl: 8.9 }) - 15.9119)// Yilmaz: 15.8475 || Matlab: 15.9119
-results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 15.5, Umschlagszahl: 8.9 }) - 13.6848)// Yilmaz: 12.3015 || Matlab: 13.6848
+results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 6.3, Umschlagszahl: 8.9 }) - 13.1330) // Yilmaz: 11.9995 || Matlab: 13.3430
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 6.3, Umschlagszahl: 8.9 }) - 13.3430) // Yilmaz: 11.9995 || Matlab: 13.3430
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 15.5, Umschlagszahl: 8.9 }) - 15.9119)// Yilmaz: 15.8475 || Matlab: 15.9119
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 15.5, Umschlagszahl: 8.9 }) - 13.6848)// Yilmaz: 12.3015 || Matlab: 13.6848
 
-// Hafen 2
-console.log("\nHafen 2")
-results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 6.3, Umschlagszahl: 1.95 }) - 17.7247)// Yilmaz: 19.2855 || Matlab: 17.7247
-results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 6.3, Umschlagszahl: 1.95 }) - 15.2017)// Yilmaz: 15.3315 || Matlab: 15.2017
-results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 15.5, Umschlagszahl: 1.95 }) - 17.9913)// Yilmaz: 19.5830 || Matlab: 17.9913
-results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 15.5, Umschlagszahl: 1.95 }) - 18.1842)// Yilmaz: 19.7550 || Matlab: 18.1842
+// // Hafen 2
+// console.log("\nHafen 2")
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 6.3, Umschlagszahl: 1.95 }) - 17.7247)// Yilmaz: 19.2855 || Matlab: 17.7247
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 6.3, Umschlagszahl: 1.95 }) - 15.2017)// Yilmaz: 15.3315 || Matlab: 15.2017
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 8, Schiffsgroesse: 15.5, Umschlagszahl: 1.95 }) - 17.9913)// Yilmaz: 19.5830 || Matlab: 17.9913
+// results.push(fuzzySystem.computeSystem({ Defektschwere: 4, Schiffsgroesse: 15.5, Umschlagszahl: 1.95 }) - 18.1842)// Yilmaz: 19.7550 || Matlab: 18.1842
 
-var sum = 0
-results.forEach((result) => sum += Math.abs(result))
-console.log(sum / 8);
+// var sum = 0
+// results.forEach((result) => sum += Math.abs(result))
+// console.log(sum / 8);
